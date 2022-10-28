f2bd3de5fa45aa392a4690949b6e901bcd941246
vra <wyf.brz@gmail.com>
2022-10-28 17:23:21 +0800

    refactor: run black

--- dompare/__init__.py
+++ dompare/__init__.py
@@ -13,28 +13,28 @@ from binaryornot.check import is_binary
 
 def create_logger(is_verbose):
     # Suppress logging info from third-party packages
-    logging.getLogger('binaryornot').setLevel(logging.ERROR)
-    logging.getLogger('chardet').setLevel(logging.ERROR)
+    logging.getLogger("binaryornot").setLevel(logging.ERROR)
+    logging.getLogger("chardet").setLevel(logging.ERROR)
 
     FIELD_STYLES = dict(
-        asctime=dict(color='red'),
-        hostname=dict(color='magenta'),
-        levelname=dict(color='yellow'),
-        filename=dict(color='magenta'),
-        name=dict(color='blue'),
-        threadName=dict(color='green')
+        asctime=dict(color="red"),
+        hostname=dict(color="magenta"),
+        levelname=dict(color="yellow"),
+        filename=dict(color="magenta"),
+        name=dict(color="blue"),
+        threadName=dict(color="green"),
     )
 
     LEVEL_STYLES = dict(
-        debug=dict(color='white'),
-        info=dict(color='cyan'),
-        warning=dict(color='red'),
-        error=dict(color='red'),
-        critical=dict(color='red')
+        debug=dict(color="white"),
+        info=dict(color="cyan"),
+        warning=dict(color="red"),
+        error=dict(color="red"),
+        critical=dict(color="red"),
     )
 
-    logger = logging.getLogger('dompare')
-    level = 'DEBUG' if is_verbose else 'INFO'
+    logger = logging.getLogger("dompare")
+    level = "DEBUG" if is_verbose else "INFO"
     logging_level = logging.DEBUG if is_verbose else logging.INFO
     logger.setLevel(logging_level)
 
@@ -42,50 +42,44 @@ def create_logger(is_verbose):
         level=level,
         fmt="[%(levelname)s] [%(asctime)s] [%(name)s] %(message)s",
         level_styles=LEVEL_STYLES,
-        field_styles=FIELD_STYLES)
+        field_styles=FIELD_STYLES,
+    )
 
     return logger
 
 
 def parse_parameters():
-    parser = argparse.ArgumentParser('dompare')
-    parser.add_argument('dir1',
-                        help="Path to the first directory")
-    parser.add_argument('dir2',
-                        help="Path to the second directory")
-    parser.add_argument('--host',
-                        type=str,
-                        default='localhost',
-                        help="host to bind")
-    parser.add_argument('--port',
-                        type=str,
-                        default=5140,
-                        help="port to listen")
-    parser.add_argument('--verbose',
-                        action='store_true',
-                        help='Show detailed information')
-    parser.add_argument('--exclude',
-                        type=str,
-                        nargs='+',
-                        help='Ignore listed directories when diff')
+    parser = argparse.ArgumentParser("dompare")
+    parser.add_argument("dir1", help="Path to the first directory")
+    parser.add_argument("dir2", help="Path to the second directory")
+    parser.add_argument("--host", type=str, default="localhost", help="host to bind")
+    parser.add_argument("--port", type=str, default=5140, help="port to listen")
+    parser.add_argument(
+        "--verbose", action="store_true", help="Show detailed information"
+    )
+    parser.add_argument(
+        "--exclude", type=str, nargs="+", help="Ignore listed directories when diff"
+    )
 
     return parser.parse_args()
 
 
 def diff_two_files(path1, path2, root_html_path):
-    content1 = ''
-    content2 = ''
-    with open(path1, encoding='utf-8') as f:
+    content1 = ""
+    content2 = ""
+    with open(path1, encoding="utf-8") as f:
         content1 = f.readlines()
-    with open(path2, encoding='utf-8') as f:
+    with open(path2, encoding="utf-8") as f:
         content2 = f.readlines()
 
     hd = difflib.HtmlDiff(tabsize=4, wrapcolumn=80)
-    diff_content = hd.make_file(content1, content2, fromdesc=path1, todesc=path2, context=True)
+    diff_content = hd.make_file(
+        content1, content2, fromdesc=path1, todesc=path2, context=True
+    )
     diff_content = remove_legends(diff_content)
 
-    if 'No Differences Found' not in diff_content:
-        with open(root_html_path.name, 'a+') as f:
+    if "No Differences Found" not in diff_content:
+        with open(root_html_path.name, "a+") as f:
             f.write(diff_content)
             f.close()
 
@@ -94,13 +88,13 @@ def diff_two_directories(logger, dir1, dir2, tmp_file, exclude):
     paths = os.listdir(dir1)
 
     if exclude is not None:
-        exclude += ['.git']
+        exclude += [".git"]
     else:
-        exclude = ['.git']
+        exclude = [".git"]
 
     for ex in exclude:
         if ex in paths:
-            logger.debug('Ignore {}'.format(ex))
+            logger.debug("Ignore {}".format(ex))
             paths.remove(ex)
 
     for path in paths:
@@ -108,25 +102,29 @@ def diff_two_directories(logger, dir1, dir2, tmp_file, exclude):
         path2 = os.path.join(dir2, path)
 
         if os.path.isdir(path1):
-            logger.debug('Processing dir {}'.format(path1))
+            logger.debug("Processing dir {}".format(path1))
             diff_two_directories(logger, path1, path2, tmp_file, exclude)
 
         elif is_binary(path1):
-            logger.debug('Ignore binary file {}'.format(path1))
+            logger.debug("Ignore binary file {}".format(path1))
             continue
 
         elif not os.path.exists(os.path.join(dir2, path)):
-            logger.debug('Ignore single file (no same name file in dir2) {}'.format(path1))
+            logger.debug(
+                "Ignore single file (no same name file in dir2) {}".format(path1)
+            )
             continue
 
         else:
-            logger.debug('Compare {} and {}'.format(path1, path2))
+            logger.debug("Compare {} and {}".format(path1, path2))
             diff_two_files(os.path.join(dir1, path), os.path.join(dir2, path), tmp_file)
 
 
 def remove_legends(content):
     """Remove redundant legends"""
-    content = content.replace('summary="Legends"', 'summary="Legends" style="display:none"')
+    content = content.replace(
+        'summary="Legends"', 'summary="Legends" style="display:none"'
+    )
     return content
 
 
@@ -137,14 +135,14 @@ def add_last_legends(tmp_file):
 
     ptn = 'style="display:none"'
     pos = content.rfind(ptn)
-    content = content[:pos] + content[pos+len(ptn):]
+    content = content[:pos] + content[pos + len(ptn) :]
 
-    with open(tmp_file.name, 'w') as f:
+    with open(tmp_file.name, "w") as f:
         f.write(content)
 
 
 def run_http_server(logger, tmp_dir, host, port):
-    logger.debug('Run http.server in dir: {}, host: {}:{}'.format(tmp_dir, host, port))
+    logger.debug("Run http.server in dir: {}, host: {}:{}".format(tmp_dir, host, port))
     os.chdir(tmp_dir)
     httpd = HTTPServer((host, int(port)), SimpleHTTPRequestHandler)
     httpd.serve_forever()
@@ -153,7 +151,9 @@ def run_http_server(logger, tmp_dir, host, port):
 def main():
     args = parse_parameters()
 
-    tmp_file = tempfile.NamedTemporaryFile(prefix='dompare-', suffix='.html', delete=False)
+    tmp_file = tempfile.NamedTemporaryFile(
+        prefix="dompare-", suffix=".html", delete=False
+    )
     tmp_dir = os.path.dirname(tmp_file.name)
 
     logger = create_logger(args.verbose)
@@ -161,13 +161,19 @@ def main():
     try:
         diff_two_directories(logger, args.dir1, args.dir2, tmp_file, args.exclude)
         add_last_legends(tmp_file)
-        url = 'http://{}:{}/{}'.format(args.host, args.port, os.path.basename(tmp_file.name))
-        logger.info('Compare finished. Please visit {} to see diff file (Press Ctrl-C to stop)'.format(url))
+        url = "http://{}:{}/{}".format(
+            args.host, args.port, os.path.basename(tmp_file.name)
+        )
+        logger.info(
+            "Compare finished. Please visit {} to see diff file (Press Ctrl-C to stop)".format(
+                url
+            )
+        )
         run_http_server(logger, tmp_dir, args.host, args.port)
     finally:
         tmp_file.close()
         os.remove(tmp_file.name)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -3,27 +3,20 @@ from setuptools import setup, find_packages
 setup(
     name="dompare",
     version="0.1.0",
-    keywords=("dompare", "Linux", 'diff', 'directories'),
+    keywords=("dompare", "Linux", "diff", "directories"),
     description="A program to diff two directories recursively",
     long_description="dompare is  program to diff same name iles in two directories recursively",
     license="MIT Licence",
-
     url="https://github.com/vra/dompare",
     author="Yunfeng Wang",
     author_email="wyf.brz@gmail.com",
-
     packages=find_packages(),
     include_package_data=True,
     platforms="any",
     install_requires=[
-        'binaryornot>=0.4.4',
-        'coloredlogs>=10.0',
+        "binaryornot>=0.4.4",
+        "coloredlogs>=10.0",
     ],
-
     scripts=[],
-    entry_points={
-        'console_scripts': [
-            'dompare=dompare.__init__:main'
-        ]
-    }
+    entry_points={"console_scripts": ["dompare=dompare.__init__:main"]},
 )
